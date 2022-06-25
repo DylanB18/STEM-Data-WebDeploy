3d6a393eac41fb9b9c80911c02733c53ca835670
Dylan Bober <deappmaker@gmail.com>
2022-06-24 21:39:54 -0400

    Added code files and data

---
+++ functions.py
@@ -0,0 +1,295 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import hashlib
+from math import nan
+
+# Config
+stem_source = 'prod_data.csv'
+DOE_source = 'DOE.csv'
+
+#Math Functions
+def calcFreeLunch(row):
+	fLunch = row['Free Lunch Eligible'] / row['Total Students']
+	return fLunch * 100
+
+def calcPercentBlack(row):
+	pBlack = row['Black Students'] / row['Total Students']
+	return pBlack * 100
+
+def calcSexRatio(row, data):
+	males = ((data['School'] == row['School Name']) & (data['Sex'] == "M")).sum()
+	females = ((data['School'] == row['School Name']) & (data['Sex'] == "F")).sum()
+
+	sRatio = round(males/females, 1)
+
+	return sRatio
+
+#Return Number of Title I Schools
+def countTitleOne(g_data):
+	return len(g_data.loc[g_data['School-wide Title I'] == "Yes"].index)
+
+#Calculate the sRatio average for Title I schools
+def calcTitleOneRatio(g_data):
+	TitleOneSchools = g_data.loc[g_data['School-wide Title I'] == "Yes"]
+	mean = TitleOneSchools['sRatio'].mean()
+	return mean
+
+def TitleOne(g_data):
+	return "The " + str(countTitleOne(g_data)) + " Title I schools have an average sexRatio of " + str(round(calcTitleOneRatio(g_data), 2))
+
+#Return Number of Title I Schools
+def countMagnet(g_data):
+	return len(g_data.loc[g_data['Magnet School'] == "Yes"].index)
+
+#Calculate the sRatio average for Title I schools
+def calcMagnetRatio(g_data):
+	TitleOneSchools = g_data.loc[g_data['Magnet School'] == "Yes"]
+	mean = TitleOneSchools['sRatio'].mean()
+	return mean
+
+def Magnet(g_data):
+	return "The " + str(countMagnet(g_data)) + " magnet schools have an average sexRatio of " + str(round(calcMagnetRatio(g_data), 2))
+
+def AllSchools(data):
+	males = (data['Sex'] == "M").sum()
+	females = (data['Sex'] == "F").sum()
+
+	sRatio = str(round(males/females, 2))
+
+	return "Countywide, the sexRatio is " + sRatio
+
+def calcRetentionDisp(school, data):
+	#Grab all students from school
+	students = data.loc[data['School'] == school]
+
+	#Seperate by gender
+	m_students = students.loc[students['Sex'] == 'M']
+	f_students = students.loc[students['Sex'] == 'F']
+
+	#Catch potential divide by zero
+	try:
+		m_avg_courses = len(m_students.index) / m_students['Student Number'].nunique()
+		f_avg_courses = len(f_students.index) / f_students['Student Number'].nunique()
+	except ZeroDivisionError:
+	    return "NaN"
+
+	return round(m_avg_courses / f_avg_courses, 2)
+
+#A version of the above function for the entire county
+def calcCRetentionDisp(data):
+	#Seperate by gender
+	m_students = data.loc[data['Sex'] == 'M']
+	f_students = data.loc[data['Sex'] == 'F']
+
+	m_avg_courses = len(m_students.index) / m_students['Student Number'].nunique()
+	f_avg_courses = len(f_students.index) / f_students['Student Number'].nunique()
+
+	return round(m_avg_courses / f_avg_courses, 2)
+
+def calcRace(data, race):
+	#Grab all students of race
+	students = data.loc[data['Race Ethnicity'] == race]
+
+	Rcount = len(students.index)
+	total = len(data.index)
+
+	return str(round(Rcount / total * 100, 2))
+
+def calcBlackWomen(data):
+	#Grab all students of race
+	black = data.loc[data['Race Ethnicity'] == "Black"]
+	bWomen = black.loc[black['Sex'] == "F"]
+
+	BWcount = len(bWomen.index)
+	total = len(data.index)
+
+	return str(round(BWcount / total * 100, 2))
+
+
+def courseCount(row, data, years):
+	name = row['School Name']
+
+	school_students = data.loc[data['School'] == name]
+
+	school_students = school_students.loc[school_students['School Year'] == 2020]
+
+	courseCount = school_students['Course Title'].nunique()
+
+	#If 2020 isn't selected, pick next best year
+	if(courseCount == 0):
+		bestYear = max(years)
+
+		school_students = data.loc[data['School'] == name]
+
+		school_students = school_students.loc[school_students['School Year'] == int(bestYear)]
+
+		courseCount = school_students['Course Title'].nunique()
+
+	return courseCount
+
+def introCourses(data):
+	#Grab students in intro courses
+	introStudents = data.loc[
+	(data['Course Title'] == "ADVANCED IT HON") |
+	(data['Course Title'] == "AP COMP SCI PRIN") |
+	(data['Course Title'] == "COMP SCI DISCOVERIES")
+	]
+
+	try:
+		#Split into male and female students
+		maleCount = introStudents['Sex'].value_counts().M
+		femaleCount = introStudents['Sex'].value_counts().F
+
+		return str(round(maleCount / femaleCount, 2))
+	except:
+		pass
+		return "Error: NaN"
+
+def nonIntroCourses(data):
+	#Grab students in advanced courses
+	advStudents = data.loc[
+	(data['Course Title'] != "ADVANCED IT HON") &
+	(data['Course Title'] != "AP COMP SCI PRIN") &
+	(data['Course Title'] != "COMP SCI DISCOVERIES")
+	]
+
+	try:
+		#Split into male and female students
+		maleCount = advStudents['Sex'].value_counts().M
+		femaleCount = advStudents['Sex'].value_counts().F
+
+		return str(round(maleCount / femaleCount, 2))
+	except:
+		pass
+		return "Error: NaN"
+
+def martin(data):
+	#Grab students in CCHS courses
+
+	introStudents = data.loc[
+	(data['Course Title'] == "AP COMP SCI PRIN")
+	]
+
+	cciStudents = introStudents.loc[
+	(introStudents['School'] == "COOPER CITY HIGH")
+	]
+	try:
+		#Split into male and female students
+		maleCount = cciStudents['Sex'].value_counts().M
+		femaleCount = cciStudents['Sex'].value_counts().F
+
+		return str(round(((maleCount / femaleCount) / ((maleCount / femaleCount) + 1) * 100), 2))
+	except:
+		pass
+		return "Error: NaN"
+
+
+def introCourseRace(data):
+	#Grab students in intro courses
+	introStudents = data.loc[
+	(data['Course Title'] == "ADVANCED IT HON") |
+	(data['Course Title'] == "AP COMP SCI PRIN") |
+	(data['Course Title'] == "COMP SCI DISCOVERIES")
+	]
+
+	#Grab all students of race
+	Wstudents = introStudents.loc[introStudents['Race Ethnicity'] == "White"]
+	Bstudents = introStudents.loc[introStudents['Race Ethnicity'] == "Black"]
+	Astudents = introStudents.loc[introStudents['Race Ethnicity'] == "Asian"]
+
+	#Count them
+	Wcount = len(Wstudents.index)
+	Bcount = len(Bstudents.index)
+	Acount = len(Astudents.index)
+
+	#Get Total
+	total = len(introStudents.index)
+
+	if(total != 0):
+		WPercent = str(round(Wcount / total * 100, 2))
+		BPercent = str(round(Bcount / total * 100, 2))
+		APercent = str(round(Acount / total * 100, 2))
+
+		return "In the same intro courses, " + WPercent + "% of students are White, " + BPercent + "% are Black, and " + APercent + "% are Asian."
+
+	else:
+		return "No students were enrolled in intro courses in the selected years, so a demographic breakdown isn't available."
+
+def yearFilter(data, years, yearList):
+	#Make empty data frame
+	filtered = pd.DataFrame(columns=data.columns)
+
+	#If for each year, if it was selected, add to data for analysis
+	for year in yearList:
+		if year in years:
+			yearData = data.loc[(data['School Year'] == int(year))]
+			filtered = filtered.append(yearData, ignore_index=True)
+
+	return filtered
+
+def t1Filter(data, DOE, titleOneMode):
+	if(titleOneMode == 'Title I Schools'):
+		#Grab T1 Schools
+		t1Schools = []
+
+		t1Only = DOE.loc[(DOE['School-wide Title I'] == "Yes")]
+
+		for index, row in t1Only.iterrows():
+			t1Schools.append(row["School Name"])
+
+		return data[data['School'].isin(t1Schools)]
+
+	elif(titleOneMode == 'Non Title I'):
+		#Grab T1 Schools
+		nTSchools = []
+
+		nTOnly = DOE.loc[(DOE['School-wide Title I'] == "No")]
+
+		for index, row in nTOnly.iterrows():
+			nTSchools.append(row["School Name"])
+
+		return data[data['School'].isin(nTSchools)]
+	else:
+		return data
+
+#Code courtesy of Heang Arngmaneekul
+def hash(sourcedf,destinationdf,*column):
+    columnName = ''
+    destinationdf['hash_'+columnName.join(column)] = pd.DataFrame(sourcedf[list(column)].values.sum(axis=1))[0].astype(str).str.encode('utf-8').apply(lambda x: (hashlib.sha224(x).hexdigest().upper()))
+
+#Loading Functions
+@st.cache
+def loadSTEMData():
+	data = pd.read_csv(stem_source)
+	return data
+
+@st.cache
+def loadDOEData():
+	data = pd.read_csv(DOE_source)
+	return data
+
+@st.cache
+def loadGraphData(main, DOE, years):
+	#Start with DOE data
+	g_data = DOE.copy()
+
+	#Calculate percents
+	g_data['% Free Lunch'] = g_data.apply(lambda row: calcFreeLunch(row), axis=1)
+	g_data['% Black'] = g_data.apply(lambda row: calcPercentBlack(row), axis=1)
+	g_data['Courses Offered'] = g_data.apply(lambda row: courseCount(row, main, years), axis=1)
+
+	#Calculate Sex Ratio
+	g_data['sRatio'] = g_data.apply(lambda row: calcSexRatio(row, main), axis=1)
+
+	sexTotal = main.groupby(["School"])["Sex"].count().reset_index()
+
+	#Discard unneeded data for organization purposes
+	g_data.drop('Free Lunch Eligible', axis=1, inplace=True)
+	g_data.drop('Hispanic Students', axis=1, inplace=True)
+	g_data.drop('Black Students', axis=1, inplace=True)
+	g_data.drop('White Students', axis=1, inplace=True)
+	g_data.drop('Two or More Races Students', axis=1, inplace=True)
+
+	return g_data
---
+++ main.py
@@ -0,0 +1,238 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import altair as alt
+import openpyxl
+from functions import calcFreeLunch, loadSTEMData, loadDOEData, loadGraphData, TitleOne, Magnet, AllSchools, calcRetentionDisp, calcCRetentionDisp, calcRace, introCourses, yearFilter, introCourseRace, nonIntroCourses, hash, calcBlackWomen, martin, t1Filter
+
+#App Config
+st.set_page_config(page_title="Broward STEM Data Analysis",
+ page_icon="📊", layout="centered", initial_sidebar_state='auto')
+
+# Years of data being analyzed
+yearList = ['2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021']
+
+# Title
+st.title('Broward STEM Data')
+st.text("All students in BCPS taking STEM Classes 2014-2021")
+
+# Load Data
+data = loadSTEMData()
+DOE_data = loadDOEData()
+
+#Choose what years to analyze
+years = st.multiselect("Years to Analyze", yearList, yearList)
+if(len(years) == 0):
+	st.text("Please select years to analyze.")
+else:
+	#Filter by years selected
+	data = yearFilter(data, years, yearList)
+
+	#Offer to sort only Title I schools
+	titleOneMode = st.radio("Analyze:", ('All Schools', 'Title I Schools', 'Non Title I'))
+
+	#Sort by Title I if requested
+	data = t1Filter(data, DOE_data, titleOneMode)
+
+	#Remove Title I or non Title I schools from DOE Data
+	if (titleOneMode == 'Title I Schools'):
+		DOE_data = DOE_data.loc[(DOE_data['School-wide Title I'] == "Yes")]
+
+	if (titleOneMode == 'Non Title I'):
+		DOE_data = DOE_data.loc[(DOE_data['School-wide Title I'] == "No")]
+
+	#Load Graph Data based on what DOE data was filtered
+	graph_data = loadGraphData(data, DOE_data, years)
+
+	#Sidebar
+	title = st.sidebar.markdown("## Menu")
+	browse = st.sidebar.markdown("[Browse Data](#raw-data)")
+	search = st.sidebar.markdown("[School Search](#school-search)")
+	graphs = st.sidebar.markdown("[Graphs](#graphs)")
+	corr = st.sidebar.markdown("[Correlations](#correlations)")
+	schoolTA = st.sidebar.markdown("[School Type Analysis](#school-type-analysis)")
+	export = st.sidebar.markdown("[Export Data](#export-data)")
+
+	#Sidebar width hack
+	st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+        width: 250px;
+    }
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+        width: 250px;
+        margin-left: -500px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+	)
+
+	# Show Raw Data
+	st.subheader('Raw Data')
+	if(st.checkbox("Show Data")):
+		st.write(data)
+
+	#Group to browse sex data by school
+	sex_data = data.groupby(["School"])["Sex"].value_counts()
+
+	st.subheader('School and Gender Breakdown')
+	if(st.checkbox("Show Breakdown")):
+		sex_data
+
+	#Search data by school
+	st.subheader("School Search")
+	selected_school = st.selectbox("Select School", DOE_data["School Name"].unique())
+	selected_row = DOE_data[DOE_data['School Name'].str.contains(selected_school)]
+
+	selected_row = selected_row.to_numpy()
+
+	st.text("Magnet: " + selected_row[0][2])
+	st.text("Title I: " + selected_row[0][3] + " (School Wide)")
+	st.text("White: " + str(round((selected_row[0][8] / selected_row[0][4])* 100, 2)) + "%")
+	st.text("Hispanic: " + str(round((selected_row[0][6] / selected_row[0][4])* 100, 2)) + "%")
+	st.text("Black: " + str(round((selected_row[0][7] / selected_row[0][4])* 100, 2)) + "%")
+	st.text("Free Lunch Eligible: " + str(round((selected_row[0][5] / selected_row[0][4])* 100, 2)) + "%")
+
+	#Sex Data Extraction
+	sex_data_arr = np.array(sex_data)
+
+	#Ignore lines 48-50 ;)
+	temp = sex_data_arr[0]
+	sex_data_arr[0] = sex_data_arr[1]
+	sex_data_arr[1] = temp
+
+	index = DOE_data[DOE_data["School Name"] == selected_school].index.values
+
+	male = sex_data_arr[index*2][0]
+	female = sex_data_arr[index*2 + 1][0]
+
+	st.text("Males per Female Student: " + str(round(male/female, 2)))
+
+	st.text("The average male student took " + str(calcRetentionDisp(selected_school, data)) + " times the STEM courses")
+	st.text("than the average female student")
+
+	#Graphs of data
+	st.subheader("Graphs")
+	st.text("Free Lunch vs. sRatio")
+	lunchSexGraph = alt.Chart(graph_data).mark_circle().encode(
+		x='% Free Lunch',
+		y='sRatio',
+		tooltip=['School Name', '% Free Lunch', 'sRatio'])
+
+	lunchSexGraph + lunchSexGraph.transform_regression('% Free Lunch', 'sRatio', method="poly").mark_line()
+
+	st.text("% Black vs. sRatio")
+	raceSexGraph = alt.Chart(graph_data).mark_circle().encode(
+		x='% Black',
+		y='sRatio',
+		tooltip=['School Name', '% Black', 'sRatio'])
+
+	raceSexGraph + raceSexGraph.transform_regression('% Black', 'sRatio', method="poly").mark_line()
+
+	st.text("Free Lunch vs. Courses Offered")
+	lunchCourseGraph = alt.Chart(graph_data).mark_circle().encode(
+		x='% Free Lunch',
+		y='Courses Offered',
+		tooltip=['School Name', '% Free Lunch', 'Courses Offered'])
+
+	lunchCourseGraph + lunchCourseGraph.transform_regression('% Free Lunch', 'Courses Offered', method="poly").mark_line()
+
+	st.text("% Black vs. Courses Offered")
+	raceCourseGraph = alt.Chart(graph_data).mark_circle().encode(
+		x='% Black',
+		y='Courses Offered',
+		tooltip=['School Name', '% Black', 'Courses Offered'])
+
+	raceCourseGraph + raceCourseGraph.transform_regression('% Black', 'Courses Offered', method="poly").mark_line()
+
+	#Remove Magnets from graph data to see if they are a source of noise
+	graph_data_nm = graph_data.loc[graph_data['Magnet School'] == "No"]
+
+	st.text("% Black vs. Courses Offered (No Magnets)")
+	raceCourseGraph_nm = alt.Chart(graph_data_nm).mark_circle().encode(
+		x='% Black',
+		y='Courses Offered',
+		tooltip=['School Name', '% Black', 'Courses Offered'])
+
+	raceCourseGraph_nm + raceCourseGraph_nm.transform_regression('% Black', 'Courses Offered', method="poly").mark_line()
+
+	st.text("School Size vs. sRatio")
+	sizeSexRatio = alt.Chart(graph_data).mark_circle().encode(
+		x='Total Students',
+		y='sRatio',
+		tooltip=['School Name', 'Total Students', 'sRatio'])
+
+	sizeSexRatio + sizeSexRatio.transform_regression('Total Students', 'sRatio', method="linear").mark_line()
+
+	#Output variable correlations
+	st.subheader("Correlations")
+	correlations = graph_data.corr(method="pearson")
+	correlations
+
+	#Crunch math by school type and output
+	st.subheader("School Type Analysis")
+
+	#These two are the average by school type with all schools having equal weight.
+	#This means a big magnet and small magnet are weighted the same
+	st.text(TitleOne(graph_data))
+	st.text(Magnet(graph_data))
+
+	#This is a true average of students
+	st.text(AllSchools(data))
+
+	st.subheader("Countywide Stats")
+
+	#Enrollment by year
+	st.text("Courses taken by year")
+	year_data = data["School Year"].value_counts().sort_index()
+	year_data
+
+	#Comparison of countywide demographics
+	st.text("Countywide, 20.2% percent of students are White,  39.3% are Black, and 3.8% are Asian. 33.7% are ethnically Hispanic.")
+	st.text("In STEM, " + calcRace(data, "White") + "% of students are White, " + calcRace(data, "Black") + "% are Black, and " + calcRace(data, "Asian") + "% are Asian. " + calcRace(data, "Hispanic") + "% are ethnically Hispanic.")
+	st.text("Black women make up " + calcBlackWomen(data) + "% of STEM Classes")
+
+	st.text("Countywide, the average male student took " + str(calcCRetentionDisp(data)) + " times more STEM courses than the average female student.")
+
+	#Analysis of intro courses
+	st.text("")
+	st.text("In CS Discoveries and AP Computer Science Principles, two popular introductory courses for students, the sRatio is just " + introCourses(data))
+	st.text(introCourseRace(data))
+	st.text("Non-intro courses had an average sRatio of " + nonIntroCourses(data))
+
+	#st.text("Mr. Martin's CS Discoveries class is " + martin(data) + "% male.")
+
+	#Data export
+	st.subheader("Export Data")
+
+	SD_CSV = st.button("Student Data (CSV)")
+	SD_EX = st.button("Student Data (Excel)")
+
+	ASD_CSV = st.button("Anonymized Student Data (CSV)")
+	ASD_EX = st.button("Anonymized Student Data (Excel)")
+
+	GD_CSV = st.button("Graph Data (CSV)")
+	GD_EX = st.button("Graph Data (Excel)")
+
+	if(SD_CSV):
+		data.to_csv("Exports/Student_Data.csv")
+	elif(SD_EX):
+		data.to_excel("Exports/Student_Data.xlsx")
+	elif(ASD_CSV):
+		a_data = data
+		hash(a_data,a_data,'Student Number','School Year')
+		a_data['Student Number'] = a_data['hash_Student NumberSchool Year']
+		a_data.drop('hash_Student NumberSchool Year', 1, inplace=True)
+		a_data.to_csv("Exports/Anon_Student_Data.csv")
+	elif(ASD_EX):
+		a_data = data
+		hash(a_data,a_data,'Student Number','School Year')
+		a_data['Student Number'] = a_data['hash_Student NumberSchool Year']
+		a_data.drop('hash_Student NumberSchool Year', 1, inplace=True)
+		a_data.to_excel("Exports/Anon_Student_Data.xlsx")
+	elif(GD_CSV):
+		graph_data.to_csv("Exports/Graph_Data.csv")
+	elif(GD_EX):
+		graph_data.to_excel("Exports/Graph_Data.xlsx")
